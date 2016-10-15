8479f2561755c355735dc65cd4b1c5e8845c6fd7
Techno11 <soren.zaiser@gmail.com>
2016-10-15 10:56:08 -0400

    Added Base And Teleop Files

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/LightningTeleop1.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/LightningTeleop1.java
@@ -3,5 +3,9 @@ package org.firstinspires.ftc.teamcode;
 /**
  * Created by SPZ Productions on 10/15/2016.
  */
-public class LightningTeleop1 {
+public class LightningTeleop1 extends LightningBase1{
+
+    public void loop(){
+
+    }
 }
