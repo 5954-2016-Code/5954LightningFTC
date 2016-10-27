9d0bfb546b5cfee2477a79b1c9d7766440b94f57
CodeRunner50 <jwthane@hotmail.com>
2016-10-26 21:43:33 -0400

    Updated some drive by gyro test code. It is trying to work but something with the encoder and angles are off.

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/LightbotAutoDriveByGyro_Linear.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/LightbotAutoDriveByGyro_Linear.java
@@ -61,7 +61,7 @@ public class LightbotAutoDriveByGyro_Linear extends LinearOpMode {
     HardwareLightbot         robot   = new HardwareLightbot();   // Use Lightbot's hardware
     //ModernRobticsI2cGyro   gyro    = null;                    // Additional Gyro device
 
-    // The IMU sensor object
+    // The Adafruit IMU sensor object
     BNO055IMU imu;
 
     // State used for updating telemetry
@@ -70,8 +70,8 @@ public class LightbotAutoDriveByGyro_Linear extends LinearOpMode {
 
     Acceleration gravity;
 
-    static final double     COUNTS_PER_MOTOR_REV    = 1440 ;    // eg: TETRIX Motor Encoder
-    static final double     DRIVE_GEAR_REDUCTION    = 2.0 ;     // This is < 1.0 if geared UP
+    static final double     COUNTS_PER_MOTOR_REV    = 280; //1440 ;    // eg: TETRIX Motor Encoder
+    static final double     DRIVE_GEAR_REDUCTION    = 1;   //2.0 ;     // This is < 1.0 if geared UP
     static final double     WHEEL_DIAMETER_INCHES   = 4.0 ;     // For figuring circumference
     static final double     COUNTS_PER_INCH         = (COUNTS_PER_MOTOR_REV * DRIVE_GEAR_REDUCTION) /
                                                       (WHEEL_DIAMETER_INCHES * 3.1415);
