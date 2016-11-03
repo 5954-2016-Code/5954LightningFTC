ad7c0b22e5c7d518c988c59826f4662def2650a0
jovanus <eric.weber.r@gmail.com>
2016-11-03 00:18:30 -0400

    More done

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/BallShooterSystem.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/BallShooterSystem.java
@@ -1,6 +1,7 @@
 package org.firstinspires.ftc.teamcode;
 
 import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.DcMotorSimple;
 import com.qualcomm.robotcore.hardware.HardwareMap;
 
 /**
@@ -9,7 +10,7 @@ import com.qualcomm.robotcore.hardware.HardwareMap;
 
 public class BallShooterSystem {
     // Ball Shooter System
-    public DcMotor  mShooterL = null,
+    private DcMotor  mShooterL = null,
                     mShooterR = null;
 
     BallShooterSystem(){
@@ -21,5 +22,19 @@ public class BallShooterSystem {
         // Ball Shooter System Init
         mShooterL = HWMap.dcMotor.get("mShooterL");
         mShooterR = HWMap.dcMotor.get("mShooterL");
+        mShooterR.setDirection(DcMotorSimple.Direction.REVERSE);
+    }
+
+    private void Power(double input){
+        mShooterL.setPower(input);
+        mShooterR.setPower(input);
+    }
+
+    public void ShootBall(){
+        Power(1.00);
+    }
+
+    public void StopShooter(){
+        Power(0.0);
     }
 }
